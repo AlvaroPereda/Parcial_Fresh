8c8d7c6aac36748767d7c6080ec355c7c05b08f7
Álvaro Pereda Sánchez <121577598+AlvaroPereda@users.noreply.github.com>
2025-04-04 10:14:11 +0000

    promises

--- routes/city/[city].tsx
+++ routes/city/[city].tsx
@@ -1,14 +1,14 @@
 import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
 import CityComponent from "../../components/CityComponent.tsx";
 import { getCapital, getCity, getWeather } from "../../utils/NinjaAPI.ts";
-import { CityComplete } from "../../utils/types.ts";
+import { CityAPI, CityComplete, CountryAPI, WeatherAPI } from "../../utils/types.ts";
 
 export const handler:Handlers = {
     GET: async(_req:Request, ctx:FreshContext<unknown, CityComplete>) => {
         const {city} = ctx.params
-        const resultCity = await getCity(city) 
-        const resultWeather = await getWeather(resultCity.latitude, resultCity.longitude)
-        const country = await getCapital(resultCity.country)
+        const resultCity:CityAPI = await getCity(city) 
+        const resultWeather:WeatherAPI = await getWeather(resultCity.latitude, resultCity.longitude)
+        const country:CountryAPI = await getCapital(resultCity.country)
         return ctx.render({
             ...resultCity,
             temp: resultWeather.temp,
